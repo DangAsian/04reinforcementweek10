ca32e1b63ea72bcad00f5bff4fbdf89a2f1d8bd4
Daniel Ang <dan.justin.ang@gmail.com>
2018-11-16 12:12:16 -0500

    completed

--- 04reinforcement_ex_week10.rb
+++ 04reinforcement_ex_week10.rb
@@ -1,66 +1,62 @@
+
 class Location
 
-  def initialize(name)
+  def  initialize (name = nil)
     @name = name
   end
-
-  ############READER############
-
-  def name
-    @name
-  end
-
-  ###########WRITER###############
 
   def name=(name)
     @name = name
+    return @name
   end
 
+  def name
+    return @name
+  end
 
 end
 
+# my_loc = Location.new ("jessore")
+#
+# puts"The old city is : #{my_loc.name}"
+#
+# my_loc.name = ('toronto')
+# puts"The new city is : #{my_loc.name}"
 
 class Trip
+  @@location = []
 
-
-  def initialize
-    @stops = []
+  def trip_loc=(trip_location)
+      @@location << trip_location
   end
 
-############READER############
-
-  def stops
-    @stops
-  end
-
-##########CODE################
-
-  def add(location)
-    @stops << location
-  end
-
-  def my_adventure
+  def get_trip
     puts "Began trip."
-    self.stops.each_cons(2) do |location|
-      puts "Travelled from #{location[0].name} to #{location[1].name}"
+    @@location.each_with_index do|dest,index|
+      if (index == @@location.length-1)
+        break
+      end
+      puts "Travelled from #{dest} to #{@@location[index+1]}"
     end
     puts "Ended trip."
   end
-
 end
 
-destination1 = Location.new("Toronto")
-destination2 = Location.new("Montreal")
-destination3 = Location.new("New Jersey")
-destination4 = Location.new("New York")
 
-my_trip = Trip.new
 
-my_trip.add(destination1)
-my_trip.add(destination2)
-my_trip.add(destination3)
-my_trip.add(destination4)
+trip_1 = Trip.new
+my_location1 =  Location.new('Toronto')
+my_location2 =  Location.new('Ottawa')
+my_location3 =  Location.new('Montreal')
+my_location4 =  Location.new('Quebec City')
+my_location5 =  Location.new('Halifax')
+my_location6 =  Location.new("St. John's")
 
-# puts my_trip.stops
+trip_1.trip_loc =(my_location1.name)
+trip_1.trip_loc =(my_location2.name)
+trip_1.trip_loc =(my_location3.name)
+trip_1.trip_loc =(my_location4.name)
+trip_1.trip_loc =(my_location5.name)
+trip_1.trip_loc =(my_location6.name)
 
-my_trip.my_adventure
+trip_1.get_trip
